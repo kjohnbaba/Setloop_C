1780742312534c47a99ad47b7a2dc03797d2234e
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2022-06-10 11:04:26 -0400

    Create Setloop.c

---
+++ Setloop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    int j;
+    
+    for(i=1;i<9;i++){
+         printf(" Set%d = ",i);
+        for(j=1;j<9;j++){
+            printf("%d ",j);
+        }
+       
+    }
+    return 0;
+}
